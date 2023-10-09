9b9408a5e7400e005d08c26023a159e7bf478b33
Maciej Michalczyk <maciejmich22@gmail.com>
2023-10-09 17:42:12 +0200

    add scrollspy

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,10 @@
 const mobileNav = document.querySelector('.nav__items--mobile');
 const hamburger = document.querySelector('.hamburger');
 const navItems = document.querySelectorAll('.nav__items--mobile .nav__item');
+const navItemsDesktop = document.querySelectorAll('.nav__items--desktop .nav__item');
 const footerYear = document.querySelector('.footer__year');
+
+const allSectionsScrollspy = document.querySelectorAll('.scrollspy');
 
 hamburger.addEventListener('click', () => {
 	mobileNav.classList.toggle('hidden');
@@ -20,3 +23,25 @@ const handleCurrentYear = () => {
 };
 
 handleCurrentYear();
+
+// Przepraszam, że ten scrollspy taki na ten moment mało "reużywalny", ale nie miałem zbytnio pomysłu na zaprojektowanie swojego, w przyszłości tutaj wrócę i zrobie uniwersalny scrollspy
+
+const removeActives = () => {
+	navItemsDesktop[0].classList.remove('active');
+	navItemsDesktop[1].classList.remove('active');
+	navItemsDesktop[2].classList.remove('active');
+};
+
+document.addEventListener('scroll', () => {
+	const currentSection = window.scrollY;
+	if (currentSection < allSectionsScrollspy[0].offsetTop + 300) {
+		removeActives();
+		navItemsDesktop[0].classList.add('active');
+	} else if (currentSection < allSectionsScrollspy[1].offsetTop + 300) {
+		removeActives();
+		navItemsDesktop[1].classList.add('active');
+	} else if (currentSection < allSectionsScrollspy[2].offsetTop + 300) {
+		removeActives();
+		navItemsDesktop[2].classList.add('active');
+	}
+});
