a2d144e943d8c6193cef9d212de31a4427946402
Maciej Michalczyk <maciejmich22@gmail.com>
2023-10-09 18:46:56 +0200

    fix scrollspy (add constes)

--- src/js/main.js
+++ src/js/main.js
@@ -24,24 +24,3 @@ const handleCurrentYear = () => {
 
 handleCurrentYear();
 
-// Przepraszam, że ten scrollspy taki na ten moment mało "reużywalny", ale nie miałem zbytnio pomysłu na zaprojektowanie swojego, w przyszłości tutaj wrócę i zrobie uniwersalny scrollspy
-
-const removeActives = () => {
-	navItemsDesktop[0].classList.remove('active');
-	navItemsDesktop[1].classList.remove('active');
-	navItemsDesktop[2].classList.remove('active');
-};
-
-document.addEventListener('scroll', () => {
-	const currentSection = window.scrollY;
-	if (currentSection < allSectionsScrollspy[0].offsetTop + 300) {
-		removeActives();
-		navItemsDesktop[0].classList.add('active');
-	} else if (currentSection < allSectionsScrollspy[1].offsetTop + 300) {
-		removeActives();
-		navItemsDesktop[1].classList.add('active');
-	} else if (currentSection < allSectionsScrollspy[2].offsetTop + 300) {
-		removeActives();
-		navItemsDesktop[2].classList.add('active');
-	}
-});
