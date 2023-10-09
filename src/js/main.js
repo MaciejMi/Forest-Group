db3a84a788f41240ea6da4ffb31ec6cdaa3eaa50
Maciej Michalczyk <maciejmich22@gmail.com>
2023-10-09 16:50:43 +0200

    finish footer

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,7 @@
 const mobileNav = document.querySelector('.nav__items--mobile');
 const hamburger = document.querySelector('.hamburger');
 const navItems = document.querySelectorAll('.nav__items--mobile .nav__item');
+const footerYear = document.querySelector('.footer__year');
 
 hamburger.addEventListener('click', () => {
 	mobileNav.classList.toggle('hidden');
@@ -12,3 +13,10 @@ navItems.forEach(item => {
 		hamburger.classList.remove('is-active');
 	});
 });
+
+const handleCurrentYear = () => {
+	const year = new Date().getFullYear();
+	footerYear.textContent = year;
+};
+
+handleCurrentYear();
