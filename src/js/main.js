955c7372e0db3bb57a8b52eb3d9b82b366d881ff
Maciej Michalczyk <maciejmich22@gmail.com>
2023-10-09 14:31:22 +0200

    add header

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,14 @@
 const mobileNav = document.querySelector('.nav__items--mobile');
 const hamburger = document.querySelector('.hamburger');
+const navItems = document.querySelectorAll('.nav__items--mobile .nav__item');
 
 hamburger.addEventListener('click', () => {
 	mobileNav.classList.toggle('hidden');
 	hamburger.classList.toggle('is-active');
+});
+navItems.forEach(item => {
+	item.addEventListener('click', () => {
+		mobileNav.classList.add('hidden');
+		hamburger.classList.remove('is-active');
+	});
 });
